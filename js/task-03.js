53d6dbbf00f3aa8d3ecd5dcb4b276d0c973c6c51
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-01 21:38:33 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listGallery = document.querySelector(".gallery");
+
+const createItems = images.map(({url, alt}) => `<li> <img src=${url} alt=${alt} width = 50%/> </li>`).join("");
+listGallery.insertAdjacentHTML(
+  "afterbegin",
+  createItems
+);
+console.log(listGallery);
+
+listGallery.style.backgroundColor = "teal";
+
