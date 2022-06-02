c52eae895afe9a54e0faca7df448b1435179e1df
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-02 10:30:47 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,11 @@
 const inputName = document.querySelector("#name-input");
 const spanName = document.querySelector("#name-output");
 
-inputName.addEventListener("input", onWriteInput);
-
-function onWriteInput(event) {
+inputName.addEventListener("input", (event) =>
+{
     if (event.currentTarget.value === "") {
-        spanName.textContent = "Anonymus"
+        spanName.textContent = "Anomymus"
     }
     else { spanName.textContent = event.currentTarget.value }
-};
+});
 
