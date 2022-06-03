b7763cfc99ea8d3c9907d98d4a6412ccae263469
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-03 13:10:58 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener('submit', onSendForm);
 
+function onSendForm(event) {
+    event.preventDefault();
+    const elementsForm = event.currentTarget.elements;
+    const email = elementsForm.email.value;
+    const password = elementsForm.password.value;
+    if (email === "" || password === "") {
+        alert("все поля должны быть заполнены !!! понял ?!")
+    }  
+    else {
+        const newForm = {
+        email, password
+        }
+        console.log(newForm);
+        event.currentTarget.reset();
+    }
+}
